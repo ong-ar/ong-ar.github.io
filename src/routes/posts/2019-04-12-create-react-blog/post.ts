9aa2011dbb6297cbd1692ef8fd9ed129124385f2
ong-ar <reclusis@gmail.com>
2019-04-12 23:48:47 +0900

    새로운 포스트 추가

---
+++ src/routes/posts/2019-04-12-create-react-blog/post.ts
@@ -0,0 +1,6 @@
+export default {
+  title: `create-react-blog`,
+  tags: ['create-react-blog', 'react', 'blog',],
+  spoiler: 'create-react-blog 프로젝트 생성 및 설정 방법',
+  getContent: () => import('./document.mdx'),
+}
