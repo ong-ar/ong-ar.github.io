e38d5d32942bf31d038bc36ae12760bc35716257
ong-ar <reclusis@gmail.com>
2019-04-12 22:56:48 +0900

    아바타 사진 추가 및 적용, 파비콘 추가 적용

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from './Bio.module.css'
 import { getGravatarURL } from '../utils/getGravatarURL'
+import avatar from "../images/avatar.jpg"
 
 interface BioProps {
   className?: string
@@ -18,9 +19,9 @@ function Bio(props: BioProps) {
       ${styles.Bio}
       ${props.className || ''}
     `}>
-      <img src={photoURL} alt="Me" />
+      <img src={avatar} alt="Me" />
       <p>
-        개발 블로그
+        웹 개발자
         <br />
         관심 기술: react, graphql(prisma), flutter
       </p>
