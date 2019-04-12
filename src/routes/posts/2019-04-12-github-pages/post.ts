a3e79d8d3cdb322bb781b7c91a99d5edb7ff4c70
ong-ar <reclusis@gmail.com>
2019-04-13 01:13:43 +0900

    new post about github pages

--- src/routes/posts/2019-04-12-github-pages/post.ts
+++ src/routes/posts/2019-04-12-github-pages/post.ts
@@ -1,6 +1,6 @@
 export default {
-  title: `create-react-blog`,
-  tags: ["create-react-blog", "react", "blog"],
+  title: `github pages`,
+  tags: ["github", "github.io", "github pages"],
   spoiler: "create-react-blog 프로젝트 생성 및 설정 방법",
   getContent: () => import("./document.mdx")
 };
