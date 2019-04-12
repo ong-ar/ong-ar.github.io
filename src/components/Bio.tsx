671eaf330db8df01a419eaa560f90fca17ae7719
ong-ar <reclusis@gmail.com>
2019-04-12 23:01:04 +0900

    불필요 파일 및 소스 제거

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import styles from './Bio.module.css'
-import { getGravatarURL } from '../utils/getGravatarURL'
 import avatar from "../images/avatar.jpg"
 
 interface BioProps {
@@ -8,11 +7,6 @@ interface BioProps {
 }
 
 function Bio(props: BioProps) {
-  let photoURL = getGravatarURL({
-    email: "reclusis@gmail.com",
-    size: 56,
-  })
-
   return (
     <div
       className={`
