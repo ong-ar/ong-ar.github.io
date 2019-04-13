72126b3d413ba07ffcb7a97b940eaf55c6d4e2bb
ong-ar <reclusis@gmail.com>
2019-04-13 23:21:37 +0900

    github pages 내용 수정

--- src/routes/posts/2019-04-12-github-pages/post.ts
+++ src/routes/posts/2019-04-12-github-pages/post.ts
@@ -1,6 +1,6 @@
 export default {
   title: `github pages`,
   tags: ["github", "github.io", "github pages"],
-  spoiler: "create-react-blog 프로젝트 생성 및 설정 방법",
+  spoiler: "github page 웹호스팅 받는 방법",
   getContent: () => import("./document.mdx")
 };
