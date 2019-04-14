5b2cd7766a082178eaa0a51ec642a0bfcd2398a7
ong-ar <reclusis@gmail.com>
2019-04-15 00:32:00 +0900

    제목 변경

--- src/routes/posts/2019-04-12-create-react-blog/post.ts
+++ src/routes/posts/2019-04-12-create-react-blog/post.ts
@@ -1,5 +1,5 @@
 export default {
-  title: `create-react-blog`,
+  title: `리액트 블로그 만들기 (create-react-blog)`,
   tags: ["create-react-blog", "react", "blog"],
   spoiler: "create-react-blog 프로젝트 생성 및 설정 방법",
   getContent: () => import("./document.mdx")
--- src/routes/posts/2019-04-12-github-pages/post.ts
+++ src/routes/posts/2019-04-12-github-pages/post.ts
@@ -1,5 +1,5 @@
 export default {
-  title: `github pages`,
+  title: `깃헙 웹호스팅 이용하기 (github pages)`,
   tags: ["github", "github.io", "github pages"],
   spoiler: "github page 웹호스팅 받는 방법",
   getContent: () => import("./document.mdx")
