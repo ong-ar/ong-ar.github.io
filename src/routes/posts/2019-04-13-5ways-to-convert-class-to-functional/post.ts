301049875f45cd5339b7e9f0c34da9016d2b1c7b
ong-ar <reclusis@gmail.com>
2019-04-15 00:27:36 +0900

    new post about react class to functional

---
+++ src/routes/posts/2019-04-13-5ways-to-convert-class-to-functional/post.ts
@@ -0,0 +1,6 @@
+export default {
+  title: `리액트, 클래스 컴포넌트에서 함수형 컴포넌트로 바꾸는 5가지 방법`,
+  tags: ["react", "class", "functional", "hooks", "component"],
+  spoiler: "state 가 있을 떄, state 가 없을 때 등등",
+  getContent: () => import("./document.mdx")
+};
