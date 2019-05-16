7797024465d25c313c7dd4e318c12fc4b8e9bf3a
ong-ar <reclusis@gmail.com>
2019-05-16 10:23:00 +0900

    react lifecycle

---
+++ src/routes/posts/2019-05-16-react-life-cycle/post.ts
@@ -0,0 +1,6 @@
+export default {
+  title: `React lifecycle`,
+  tags: ["react", "lifecycle"],
+  spoiler: "mounting, updating, props change, state change lifecycle",
+  getContent: () => import("./document.mdx")
+};
